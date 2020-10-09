ca2fc0e673b6f6c36e31ccd889213e9aa5293e40
ganirockz <ganeshrapeti8@gmail.com>
2020-10-09 07:30:04 +0530

    [Ganesh]Added: Given three Float numbers find max

--- src/test/java/com/capgemini/MaximumTest.java
+++ src/test/java/com/capgemini/MaximumTest.java
@@ -5,12 +5,11 @@ public class MaximumTest {
 	@Test
 	public static void validateMaxMethod_whenGivenThreeIntegers_ShouldReturnMax() {
 		Maximum m = new Maximum();
-		Integer a1=10,a2=10,a3=30;
-		Integer b1=20,b2=30,b3=10;
-		Integer c1=30,c2=20,c3=20;
-		Integer max = m.getMax(a2, b2, c2);
-		Integer d = 30;
-		Assert.assertTrue((d.equals(m.getMax(a1, b1, c1))&&(d.equals(max))
+		Float a1=(float) 10.23,a2=(float)10.23,a3=(float)30;
+		Float b1=(float)20,b2=(float)30,b3=(float)10;
+		Float c1=(float)30,c2=(float)20,c3=(float)20;
+		Float d = (float)30;
+		Assert.assertTrue((d.equals(m.getMax(a1, b1, c1))&&(d.equals(m.getMax(a2, b2, c2)))
 				&&(d.equals(m.getMax(a3, b3, c3)))));
 	} 
 }
